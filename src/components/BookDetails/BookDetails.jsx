43db7bdced1a0a73294747809b1c02fda9c7052e
Dina Rosita <rositadina816@gmail.com>
2023-06-24 11:51:42 +0700

    fitur search

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+
+
 import { useParams } from 'react-router-dom';
 import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,25 +4,30 @@ import aboutImg from "../../images/about-img.jpg";
 
 const About = () => {
   return (
-    <section className='about'>
-      <div className='container'>
-        <div className='section-title'>
+    <section className="about">
+      <div className="container">
+        <div className="section-title">
           <h2>About</h2>
         </div>
 
-        <div className='about-content grid'>
-          <div className='about-img'>
-            <img src = {aboutImg} alt = "" />
+        <div className="about-content grid">
+          <div className="about-img">
+            <img src={aboutImg} alt="" />
           </div>
-          <div className='about-text'>
-            <h2 className='about-title fs-26 ls-1'>About BookHub</h2>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dignissimos consequuntur vero commodi provident maiores, iusto atque corrupti voluptate vel sequi consectetur unde aliquam corporis saepe animi non, tempora reiciendis molestias sed laudantium dolores. Assumenda aperiam fuga quo voluptate commodi ullam aliquam expedita voluptas delectus.</p>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dicta, possimus inventore eveniet atque voluptatibus repellendus aspernatur illo aliquam dignissimos illum. Commodi, porro omnis dolore amet neque modi quas eum!</p>
+          <div className="about-text">
+            <h2 className="about-title fs-26 ls-1">Generasi E-BOOK</h2>
+            <p className="fs-17">
+              Selamat datang di era revolusi literasi: generasi E-book. Dalam dunia yang semakin terhubung secara digital, E-book telah mengubah cara kita membaca dan mengakses informasi. Dengan hanya perangkat elektronik di tangan kita,
+              perpustakaan pribadi penuh dengan buku-buku favorit kita dapat dibawa ke mana saja. Dari klasik hingga terbitan terbaru, semua itu hanya dalam genggaman. Tidak hanya memberikan kenyamanan membaca di mana saja dan kapan saja,
+              generasi E-book juga membuka pintu bagi interaktivitas, dengan fitur-fitur seperti pencarian teks dan anotasi. Ini adalah era di mana imajinasi bertemu dengan teknologi, dan penulis serta karya-karya baru diberi kesempatan
+              untuk bersinar. <br></br> Generasi E-book adalah keajaiban digital yang mengubah cara kita melibatkan diri dalam dunia kata-kata, menawarkan kebebasan, kemudahan, dan petualangan tak terbatas di setiap halaman. Selamat datang di era
+              E-book, di mana cerita tak berujung menanti untuk ditemukan.
+            </p>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default About
